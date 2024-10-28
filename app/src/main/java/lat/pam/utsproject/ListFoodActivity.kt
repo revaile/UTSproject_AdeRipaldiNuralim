e9ca7a64bf61eb6e1e2236956fb20a917ba8e51e
Ade Ripaldi <ripaldinuralim@gmail.com>
2024-10-28 15:55:17 +0700

    change description

--- app/src/main/java/lat/pam/utsproject/ListFoodActivity.kt
+++ app/src/main/java/lat/pam/utsproject/ListFoodActivity.kt
@@ -25,19 +25,17 @@ class ListFoodActivity : AppCompatActivity() {
 
         // Menyiapkan data makanan
         foodList = listOf(
-            Food("Batagor", "Batagor asli enak dari Bandung", R.drawable.batagor),
-            Food("Black Salad", "Salad segar yang dibuat secara langsung", R.drawable.black_salad),
-            Food("Cappucino", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.cappuchino),
-            Food("Chese Cake", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.cheesecake) ,
-            Food("Cireng", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.cireng),
-            Food("Donut", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.donut) ,
-            Food("Kopi Hitam", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.kopi_hitam),
-            Food("Mie Goreng", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.mie_goreng),
-            Food("Nasi Goreng", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.nasigoreng),
-            Food("Sparkling Tea", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.sparkling_tea),
-
-
-            )
+            Food("Batagor", "Batagor asli khas Bandung, gurih dan renyah dengan saus kacang lezat", R.drawable.batagor),
+            Food("Black Salad", "Salad segar dan sehat, kombinasi sayuran hijau dan buah-buahan premium", R.drawable.black_salad),
+            Food("Cappuccino", "Kopi Cappuccino autentik dari biji kopi Arabica terbaik, dengan aroma khas", R.drawable.cappuchino),
+            Food("Cheese Cake", "Kue keju lembut dengan topping krim yang menggoda selera", R.drawable.cheesecake),
+            Food("Cireng", "Cireng khas Bandung, renyah di luar dan kenyal di dalam, cocok untuk camilan", R.drawable.cireng),
+            Food("Donut", "Donat manis dengan berbagai pilihan topping lezat dan menggiurkan", R.drawable.donut),
+            Food("Kopi Hitam", "Kopi hitam pekat dengan rasa yang kuat, dibuat dari biji kopi pilihan", R.drawable.kopi_hitam),
+            Food("Mie Goreng", "Mie goreng dengan bumbu khas dan rempah-rempah yang kaya, mantap dinikmati kapan saja", R.drawable.mie_goreng),
+            Food("Nasi Goreng", "Nasi goreng spesial dengan campuran sayuran dan daging, favorit semua orang", R.drawable.nasigoreng),
+            Food("Sparkling Tea", "Teh bersoda yang segar dan unik, perpaduan rasa teh dengan sentuhan sparkling", R.drawable.sparkling_tea)
+        )
 
         adapter = FoodAdapter(foodList)
         recyclerView.adapter = adapter
